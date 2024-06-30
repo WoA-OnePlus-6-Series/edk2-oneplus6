82d16bf9d29a84c72080b8fc71fcf72a0732a2d8
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-06-30 15:00:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -199,6 +199,7 @@ then	set -e
 		popd
                 cp ${PWD}/assets/header.makefile ${PWD}/edk2/BaseTools/Source/C/Makefiles
                 cp ${PWD}/fixedfiles/stb_c_lexer.h ${PWD}/sdm845Pkg/Library/SimpleInit/libs/stb
+                cp ${PWD}/fixedfiles/lib.c ${PWD}/sdm845Pkg/Library/SimpleInit/src/gui/engine
 	fi
 	set +e
 fi
